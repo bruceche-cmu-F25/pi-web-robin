8f1eb4e6696b42b827536bcfd88bd2f7fc963c17
Bruce Cheng <bruceche@andrew.cmu.edu>
2026-09-09 00:26:19 -0700

    chore(deps): update Pi SDK packages to 0.85.1

--- lib/rpc-manager.ts
+++ lib/rpc-manager.ts
@@ -159,7 +159,7 @@ const THINKING_LEVEL_NAMES = new Set<ThinkingLevel>(["off", "minimal", "low", "m
 class PlainTextTheme extends Theme {
   constructor() {
     super(
-      { thinkingXhigh: "", searchMatchText: "" } as ConstructorParameters<typeof Theme>[0],
+      { thinkingXhigh: "", searchMatchText: "", scrollbarTrack: "", scrollbarThumb: "" } as ConstructorParameters<typeof Theme>[0],
       { selectedBg: "" } as ConstructorParameters<typeof Theme>[1],
       "truecolor",
     );
